e8da8e09ee5263d4abd2dd58c90eb97e0798cd55
A1exsandro <alexsandro.acp@gmail.com>
2022-05-17 14:28:13 -0400

    component screenshotButton

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -6,12 +6,48 @@ import {
     Text,
     TouchableOpacity
 } from 'react-native';
+import { ArrowLeft } from 'phosphor-react-native';
+
+import { FeedbackType } from '../Widget';
 
 import { styles } from './styles';
+import { theme } from '../../theme';
+import { feedbackTypes } from '../../utils/feedbackTypes'
 
-export function Form() {
+interface Props {
+    feedbackType: FeedbackType;
+}
+
+export function Form({feedbackType}: Props) {
+    const feedbackTypeInfo = feedbackTypes[feedbackType];
     return (
         <View style={styles.container}>
+            <View style={styles.header}>
+                <TouchableOpacity>
+                    <ArrowLeft 
+                        size={24}
+                        weight="bold"
+                        color={theme.colors.text_secondary}
+                    />
+                </TouchableOpacity>
+
+                <View style={styles.titleContainer}>
+                    <Image 
+                        source={feedbackTypeInfo.image}
+                        style={styles.image}
+                    />
+                    <Text style={styles.titleText}>
+                        {feedbackTypeInfo.title}
+                    </Text>
+                </View>
+            </View>
+
+            <TextInput 
+                multiline
+                style={styles.input}
+                placeholder="Algo não está funcionando bem? Queremos corrigir. Conte com detalhe o que está acontecendo."
+                placeholderTextColor={theme.colors.text_secondary}
+            />
 
         </View>
     )
