f69379f788504e040c398072d5f288eff089264a
A1exsandro <alexsandro.acp@gmail.com>
2022-05-17 15:29:47 -0400

    component Success

---
+++ src/components/Success/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View } from 'react-native';
+import { styles } from './styles';
+
+export function Success() {
+    return (
+        <View style={styles.container}></View>
+    );
+}
