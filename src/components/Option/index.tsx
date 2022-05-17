65d4a5e67186e217e91f5124625792a7060fc6cb
A1exsandro <alexsandro.acp@gmail.com>
2022-05-17 13:09:11 -0400

    component form

--- src/components/Option/index.tsx
+++ src/components/Option/index.tsx
@@ -1,13 +1,33 @@
 import React from 'react';
-import { View } from 'react-native';
-import { Copyright } from '../Copyright';
+import { 
+    TouchableOpacity,
+    TouchableOpacityProps,
+    Image,
+    ImageProps,
+    Text
+ } from 'react-native';
 
 import { styles } from './styles';
 
-export function Option() {
+interface Props extends TouchableOpacityProps {
+    title: string;
+    image: ImageProps;
+}
+
+export function Option({ title, image, ...rest }: Props) {
     return (
-        <View style={styles.container}>
-            
-        </View>
+        <TouchableOpacity 
+        style={styles.container}
+        {...rest}
+        >
+            <Image 
+                source={image}
+                style={styles.image}
+            />
+
+            <Text style={styles.title}>
+                {title}
+            </Text>
+        </TouchableOpacity>
     )
 }
